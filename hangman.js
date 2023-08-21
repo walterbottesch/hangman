17dd0145eb1b7803e2e8a701f713102cb3061eea
Walter Bottesch <124290515+walterbottesch@users.noreply.github.com>
2023-08-21 11:09:44 +0100

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -4,14 +4,13 @@ var messageBox  = document.getElementById("display");
 
 var wordToGuess = chooseWord();
 var guessedChar = 0;
-var wordToGuess1 = [wordToGuess.length];
+var wordToGuessLength = [wordToGuess.length];
 
 
 console.log(wordToGuess);
-console.log("w = " + wordToGuess1);
+console.log("w = " + wordToGuessLength);
 
 var lives = 7;
-var status;
 
 function chooseWord() {
 	var indexWord = Math.floor(Math.random() * dictionary.length);
@@ -21,7 +20,7 @@ function chooseWord() {
 function createUnderscore(word) {
 	var table = document.createElement('table');
 	var tr = document.createElement('tr');
-	for(var i = 0; i < wordToGuess.length; i++) {
+	for(var i = 0; i < wordToGuessLength; i++) {
 		var td = document.createElement('td');
 		td.innerHTML = "_";
 		td.setAttribute("id", i );
@@ -34,40 +33,15 @@ function check() {
 	var userGuessChar = document.getElementById("userGuess");
 	var userGuess = userGuessChar.value;
 	var indexes = getAllIndexes(wordToGuess, userGuess);
-	showChar(indexes, userGuess);
+	if(indexes.length > 0) {
+		replaceUnderscore(indexes,userGuess );
+	} else {
+		lives--;
+	}
+	showLives();
 	displayStatus();
 	userGuessChar.value = "";
 }
-
-function showLives() {
-	var displayLives = document.getElementById("lives");
-	displayLives.innerHTML = "Lives " + lives;
-}
-
-function displayStatus() {
-	var displayWin = document.getElementById("display");
-	if(lives === 0) {
-		showLives();
-		displayWin.innerHTML = "You Loose";
-		
-	} else {
-		showLives();
-	}
-}
-
-
-function showChar(indexes, charGuess) {
-	for(var i = 0; i < indexes.length; i++) {
-		var charPos = document.getElementById(indexes[i]);
-		if(charPos.innerHTML !=="_") {
-			lives--;
-		} else {
-			charPos.innerHTML = charGuess;
-		}
-		
-	}
-}
-
 function getAllIndexes(arr, val) {
     var indexes = [];
     for(var i = 0; i < arr.length; i++)
@@ -77,7 +51,45 @@ function getAllIndexes(arr, val) {
 		}
     return indexes;
 }
-
+function replaceUnderscore(indexes, charGuess) {
+	for(var i = 0; i < indexes.length; i++) {
+		var charPos = document.getElementById(indexes[i]);
+		if(charPos.innerHTML === "_") {
+			charPos.innerHTML = charGuess;
+		} else {
+			lives--;
+		}
+	}
+}
+function showLives() {
+	var displayLives = document.getElementById("lives");
+	displayLives.innerHTML = "Lives " + lives;
+}
+function checkWin() {
+	var charMissing = 1;
+	for(var i = 0; i < wordToGuessLength; i++) {
+		var a = document.getElementById(i)
+		var charGuessed = a.innerHTML;
+		if(charGuessed === "_") {
+			charMissing = 0;
+		} 
+	}
+	return charMissing;
+}
+function displayStatus() {
+	var displayWin = document.getElementById("display");
+	var checkBtn = document.getElementById("checkButton");
+	var userGuess = document.getElementById("userGuess");
+	if(lives > 0 && checkWin()) {
+			displayWin.innerHTML = " Congratulations! You've got it !";
+			checkBtn.disabled = true;
+			userGuess.disabled = true;
+	} else if(lives === 0) {
+		displayWin.innerHTML = "You Loose";
+		checkBtn.disabled = true;
+		userGuess.disabled = true;
+	}
+}
 function init() {
 	console.log(wordToGuess);
 	createUnderscore();
